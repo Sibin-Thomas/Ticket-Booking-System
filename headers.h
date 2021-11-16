ae9a2ab1b914e0c8706654d90cbdd7bd9c40d64e
Sibin-Thomas <sibintho007@gmail.com>
2021-11-16 23:44:21 +0530

    added headers

---
+++ headers.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h> 
+#include <unistd.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <string.h>
+#include <fcntl.h>
